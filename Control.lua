33f60b978ac30692ab8c5c5a767b6323994d9e0d
powe0101 <powe0101@naver.com>
2016-07-21 17:04:02 +0900

    --
    --

--- Control.lua
+++ Control.lua
@@ -5,7 +5,7 @@ MOVE_POWER = 1
 
 function BackgroundMove(_s,dt) --07 21 근영 key 입력 받았을시 
 self=_s --tree,cloud에서 self를 받는다 
-	if love.keyboard.isDown('right')and  pl:GetX()~=590 then
+	if love.keyboard.isDown('right')and  pl:GetX()~=590 and pl:GetX() ~= 0 then
 		self.frame = (self.frame + 15*dt) % 3
 		
 			  if isCanMove then
