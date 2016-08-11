dcc01bfd09b6e97894168c19a1323c04ad92a9b2
powe0101 <powe0101@naver.com>
2016-08-11 10:59:31 +0900

    노티스 추가
    -

--- StageSpring.lua
+++ StageSpring.lua
@@ -32,6 +32,8 @@ function CreateSpring()
   stageSpring = StageSpring.Create()
 
   stageSpring:CreateDustWind()
+  notice = Notice.Create()
+  notice:SetText("Stage Spring")
 
   CreateGround(-177,76) --도개교가 깔리고 그 아래 강물이 생길거니까 플레이어로 부터 얻은 좌표 기준으로 290이상 못가게 막아야 함.
   CreateGround(600,76)
