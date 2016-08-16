1f9ab8893a25a383b1c79739f7a3bedcad70c4bd
geunyoung1 <y0176@naver.com>
2016-08-16 21:49:30 +0900

    stageSummer 수정

--- StageSummer.lua
+++ StageSummer.lua
@@ -16,13 +16,22 @@ function CreateSummer()
     CreatePicket(200,400)      
     CreateCloud(300,0)
     CreateCloud(350,100)  
-    CreateCloud(400,200)
 
-    CreateGround(-49,430) 
-    CreateGround(541,430)
-    CreateCreeper(-49,430)
-    CreateCreeper(541,430)
 
+
+    CreateGround(-89,440) 
+    CreateGround(521,440)
+    CreateCreeper(-89,440)
+    CreateCreeper(521,440)
+
+
+ 
+
+    CreateCloud(402,200)
+    CreateBox(410,220)
+    CreateBox(420,220)
+    CreateBox(430,220) 
+    CreateBox(440,220)
 
     CreateQMark(255, 100) 
     CreatePicket(255,115)
@@ -31,6 +40,7 @@ function CreateSummer()
     CreateBox(250,140)
     CreateBox(260,140)
     CreateBox(270,140) 
+    CreateBox(280,140)
 
 
    
