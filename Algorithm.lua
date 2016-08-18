65bac9a021c5eec3714c225269d2bc69a6e03954
GeneLee <dlgustlr45@naver.com>
2016-08-18 10:53:59 +0900

    마스터 합병
    하기위한 커밋

--- Algorithm.lua
+++ Algorithm.lua
@@ -18,7 +18,7 @@ function SplitBackground()
 	--558을 4:4:2 비율로 쪼개면 223.2, 111.6
 	love.graphics.setColor(0,0,0,255) -- 검은색 RGBA
 
-	love.graphics.rectangle("fill", 62+200, 12, 2, 166) --선처럼 생긴 사각형을 만들기.
+	--love.graphics.rectangle("fill", 62+200, 12, 2, 166) --선처럼 생긴 사각형을 만들기.
 	love.graphics.rectangle("fill", 62+400, 12, 2, 166) --선처럼 생긴 사각형을 만들기.
 
 	--love.graphics.line(62+200, 12, 62+200, 178)
