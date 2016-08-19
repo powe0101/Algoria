d90d78dcabc125348a91c6a2a3a69b710ffb1a85
CurrentReal <lhj672400@naver.com>
2016-08-19 13:42:07 +0900

    가을배경추가

--- BackGround.lua
+++ BackGround.lua
@@ -12,7 +12,7 @@ function BackGround.create()
 end
 
 function BackGround:reset(x,y)
-	--0808 근영 yfix yspeed 추가 
+	--0808 근영 yfix yspeed 추가
 	self.yfix=y
 	self.yspeed = 0
 	self.onGround = true
@@ -25,20 +25,20 @@ function BackGround:reset(x,y)
 	backGround_now_frame = backGround_frames_x[0]
 end
 
-function BackGround:SpaceJump(dt) --0808근영 점프함수  
+function BackGround:SpaceJump(dt) --0808근영 점프함수
 	self=SCheckSpaceBarDown(self,dt)
 end
 
-function BackGround:normal(dt) --cloud 이동 
+function BackGround:normal(dt) --cloud 이동
 	self=BackgroundNormal(self,dt)
 end
 
-function BackGround:UpdateMove(dt) --cloud key이벤트 
+function BackGround:UpdateMove(dt) --cloud key이벤트
 	self=BackgroundMove(self,dt)
 end
 
 function BackGround:update(dt)
-	if stageLevel==2 then -- 0808근영 여름 스테이지 에서 점프함수 호출 
+	if stageLevel==2 then -- 0808근영 여름 스테이지 에서 점프함수 호출
 		self:SpaceJump(dt)
 	end
 	self:UpdateMove(dt)
@@ -49,13 +49,14 @@ function BackGround:draw()
 	if stageLevel==0 then
 		backGround_frames_x[0]  = love.graphics.newQuad(0,0,300,200,300,200)
 		love.graphics.draw(imgVillageBackGround,backGround_frames_x[0],self.x,self.y)
-	elseif stageLevel==1 then 
+	elseif stageLevel==1 then
 		backGround_frames_x[0]  = love.graphics.newQuad(0,0,600,200,600,200)
-			
 		love.graphics.draw(imgSpringBackGround,backGround_frames_x[0],self.x,self.y)
-	elseif stageLevel==4 then 
+	elseif stageLevel==3 then
+		backGround_frames_x[0] = love.graphics.newQuad(0,0,600,200,600,200)
+		love.graphics.draw(imgFallBackGround,backGround_frames_x[0],self.x,self.y)
+	elseif stageLevel==4 then
 		backGround_frames_x[0]  = love.graphics.newQuad(0,0,600,200,600,200)
-			
 		love.graphics.draw(imgWinterBackGround,backGround_frames_x[0],self.x,self.y)
     end
 end
