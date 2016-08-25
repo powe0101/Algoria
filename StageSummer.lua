971b9d0b19ae58a962458d5c747c2a7f6f1cfe83
CurrentReal <lhj672400@naver.com>
2016-08-25 11:49:50 +0900

    라인 수정

--- StageSummer.lua
+++ StageSummer.lua
@@ -10,8 +10,8 @@ function CreateSummer()
     pl:StartSummerStage()
 
     CreateBackGround(0,0)
-	CreateCloud(100,200)
-	CreateCloud(200,400)
+		CreateCloud(100,200)
+		CreateCloud(200,400)
     CreatePicket(200,400)
     CreateCloud(300,0)
     CreateCloud(350,100)
@@ -21,11 +21,8 @@ function CreateSummer()
     CreateGround(421,430)
     CreateTree(100,890)
 
-
-
     CreateCreeper(-79,430)
     CreateCreeper(500,430)
-
 
     CreateQMark(255, 100)
     CreatePicket(255,115)
@@ -34,16 +31,15 @@ function CreateSummer()
     CreateBox(250,140)
     CreateBox(260,140)
     CreateBox(270,140)
-    --pl:StartSummerStage() --스테이지가 변경됐을때 초기좌표로 되돌리기 위한 메서드
-       CreateCloud(100,460)
-         CreateCloud(100,500)
-         CreateCloud(100,600)
-         CreateCloud(100,700)
-         CreateCloud(100,800)
-         CreateCloud(100,900)
-         CreateCloud(100,1000)
-                  CreateCloud(100,1100)
-                           CreateCloud(100,1200)
 
+    CreateCloud(100,460)
+    CreateCloud(100,500)
+    CreateCloud(100,600)
+		CreateCloud(100,700)
+    CreateCloud(100,800)
+    CreateCloud(100,900)
+    CreateCloud(100,1000)
+    CreateCloud(100,1100)
+    CreateCloud(100,1200)
 	end
 end
