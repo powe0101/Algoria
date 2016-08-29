aed496b851237cb07cba199a99b04df299423b26
geunyoung1 <y0176@naver.com>
2016-08-29 10:43:08 +0900

    겨울 스테이지 추가

--- Control.lua
+++ Control.lua
@@ -8,28 +8,28 @@ function BackgroundMove(_s,dt) --07 21 근영 key 입력 받았을시
   self=_s --tree,cloud에서 self를 받는다
 
     if love.keyboard.isDown('right') then
-      if pl and pl:GetX()~=WIDTH-10 and pl:GetX()~=0 and pl:GetX() > 225 + BridegePassValue and stageLevel == 1 then --봄일 때.
+      if pl and pl:GetX()~=WIDTH-30 and pl:GetX()~=0 and pl:GetX() > 225 + BridegePassValue and stageLevel == 1 then --봄일 때.
         -- 도개교가 열리기 전까지는 못움직이게 함. 이후 움직이게 하기 위해서는 TRUE/FALSE로 값을 조절해야 할 듯.
         if canPass and pl:GetX()~=WIDTH-10 and pl:GetX()~=0 then -- 이 사이에 있는 부분을 메서드로 빼면 좋을 것 같은데 방법이 없나? 계속 터지네.. by.현식 0728
            self.frame = (self.frame + 15*dt) % 3
               self.x = self.x - MOVE_POWER
         end -- canPass
  
-      elseif pl and pl:GetX()~=WIDTH-10 and pl:GetX()~=0 and isCanMoveRight then
+      elseif pl and pl:GetX()~=WIDTH-30 and pl:GetX()~=0 and isCanMoveRight then
           self.frame = (self.frame + 15*dt) % 3
           self.x = self.x - MOVE_POWER
       end
     end
 
     if love.keyboard.isDown('left')  then
-      if pl and pl:GetX()~=WIDTH-10 and pl:GetX()~=0 and pl:GetX() < 460 - BridegePassValue and stageLevel == 3 then
+      if pl and pl:GetX()~=WIDTH-30 and pl:GetX()~=0 and pl:GetX() < 460 - BridegePassValue and stageLevel == 3 then
         -- 도개교가 열리기 전까지는 못움직이게 함. 이후 움직이게 하기 위해서는 TRUE/FALSE로 값을 조절해야 할 듯.
         if canPass then -- 이 사이에 있는 부분을 메서드로 빼면 좋을 것 같은데 방법이 없나? 계속 터지네.. by.현식 0728
           self.frame = (self.frame + 15*dt) % 3
           self.x = self.x + MOVE_POWER
         end -- canPass
  
-      elseif pl and pl:GetX()~=WIDTH-10 and pl:GetX()~=0 and isCanMoveLeft then
+      elseif pl and pl:GetX()~=WIDTH-30 and pl:GetX()~=0 and isCanMoveLeft then
         self.frame = (self.frame + 15*dt) % 3
         self.x = self.x + MOVE_POWER
       end
@@ -42,8 +42,14 @@ function BackgroundNormal(_s,dt) --07 21 근영 background 이동
 
   if self.status == 0   then -- normal ourside
    self.y = self.y - self.yspeed*dt
-    if checkPlaying==false and groundList[0]:GetY()>260 and canPass then -- 여름 스테이지에서 문제를 다 풀고 가시애니메이션이 실행 후
-    	return self
+    if checkPlaying==false and groundList[0]:GetY()>260 and canPass and stageLevel==2 then -- 여름 스테이지에서 문제를 다 풀고 가시애니메이션이 실행 후
+    	
+      return self
+    elseif stageLevel==4 and canPass and groundList[0]:GetY()<260  then
+        self.yspeed=-300
+
+      return self
+
     elseif stageLevel==2 and pl:GetIsTop() and pl:GetIsBottom() then--상자 위에 올라 갔을때
     
           self.yspeed=0
