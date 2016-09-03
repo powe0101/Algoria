70df7ce55f329734689f8cb23e0f68441dde0eca
powe0101 <powe0101@naver.com>
2016-09-03 19:54:04 +0900

    코인

--- Coin.lua
+++ Coin.lua
@@ -9,16 +9,16 @@ function Coin.Create()
 end
 
 function Coin:Reset()
-  self.point = 0
-  self.coin = 0
-  self.count = 0
+  self.point = 0 -- 동전 * 돈
+  self.coin = 0 -- 저금통에 저장된 돈
+  self.count = 0 -- 저금통에 저장된 동전 개수
   self.width = 300
   self.height = 150
-  
+  self.random = 0  -- Random 값으로 동전 선택 0~2 (50,100,500)
 end
 
 function Coin:Play()
-
+  --애니메이션 재생
 end
 
 function Coin:Stop()
