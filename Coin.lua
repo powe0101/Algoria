d24e5aba3c7d6d20f0a1bb182a8e2936ddbff2a6
powe0101 <powe0101@naver.com>
2016-09-05 10:51:25 +0900

    봄 스테이지 코인
    -

--- Coin.lua
+++ Coin.lua
@@ -1,6 +1,8 @@
 Coin = {}
 Coin.__index = Coin
 
+coin_frame = love.graphics.newQuad(0, 0, 32, 32, 32, 32)
+
 function Coin.Create()
   local self = {}
   setmetatable(self,Coin)
@@ -9,24 +11,40 @@ function Coin.Create()
 end
 
 function Coin:Reset()
-  self.point = 0 -- 동전 * 돈
-  self.coin = 0 -- 저금통에 저장된 돈
-  self.count = 0 -- 저금통에 저장된 동전 개수
-  self.width = 300
-  self.height = 150
-  self.random = 0  -- Random 값으로 동전 선택 0~2 (50,100,500)
+  self.width = 32
+  self.height = 32
+  self.point = Coin:SelectCoinRandom()  -- Random 값으로 동전 선택 0~2 (10,50,100)
+  self.visible = true
+
+  self:RegisterImage(self.point)
 end
 
-function Coin:Play()
-  --애니메이션 재생
+function Coin:RegisterImage(_point)
+  if _point == 10 then
+
+  elseif _point == 50 then
+
+  elseif _point == 100 then
+
+  end
+
 end
 
-function Coin:Stop()
+function Coin:SelectCoinRandom()
+  local random = randomSeed:newRandomGenerator(0,2)
+  print("선택된 코인 랜덤 값 : "..random)
 
+  if random == 0 then
+    return 10
+  elseif random == 1 then
+    return 50
+  elseif random == 2 then
+    return 100
+  end
 end
 
 function Coin:Draw()
-
+  love.graphics.draw(imgCoin,coin_frame,0,WIDTH/2)
 end
 
 function Coin:Update()
