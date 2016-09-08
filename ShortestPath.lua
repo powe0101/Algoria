606f2d0a0c491e1b4b9f074666861885d33d1c01
CurrentReal <lhj672400@naver.com>
2016-09-08 20:59:36 +0900

    최단거리 알고리즘 패턴 단! 하나 그리기완료

--- ShortestPath.lua
+++ ShortestPath.lua
@@ -17,6 +17,21 @@ STARTHOUSE = 1
 CHECKHOUSE = 2
 ENDHOUSE = 3
 
+FIRSTHOUSE_X = 70+20
+SECONDHOUSE_x = 160+20
+THIRDHOUSE_X = 250+20
+
+FIRSTHOUSE_Y = 25
+SECONDHOUSE_Y = 75
+THIRDHOUSE_Y = 125
+
+HOUSE_SIZE = 32
+HOUSE_HALFSIZE = 16
+
+PATH_X = 58
+PATH_Y = 18
+PATH_SIZE = 5
+
 -- 최단 경로 저장 배열
 dist = {0, INFINITE, INFINITE, INFINITE, INFINITE, INFINITE, INFINITE, INFINITE}
 -- 방문 여부 표현
@@ -62,32 +77,51 @@ function ShortestPath()
 end
 
 function InitShortestPath()
-  CreateShortestPathtHouse(70, 25, STARTHOUSE)
-  CreateShortestPathtHouse(70, 75, CHECKHOUSE)
-  CreateShortestPathtHouse(70, 125, CHECKHOUSE)
+  CreateShortestPathtHouse(FIRSTHOUSE_X, FIRSTHOUSE_Y, STARTHOUSE)
+  CreateShortestPathtHouse(FIRSTHOUSE_X, SECONDHOUSE_Y, CHECKHOUSE)
+  CreateShortestPathtHouse(FIRSTHOUSE_X, THIRDHOUSE_Y, CHECKHOUSE)
 
-  CreateShortestPathtHouse(160, 25, CHECKHOUSE)
-  CreateShortestPathtHouse(160, 75, CHECKHOUSE)
+  CreateShortestPathtHouse(SECONDHOUSE_x, FIRSTHOUSE_Y, CHECKHOUSE)
+  CreateShortestPathtHouse(SECONDHOUSE_x, SECONDHOUSE_Y, CHECKHOUSE)
 
-  CreateShortestPathtHouse(250, 25, CHECKHOUSE)
-  CreateShortestPathtHouse(250, 75, CHECKHOUSE)
-  CreateShortestPathtHouse(250, 125, ENDHOUSE)
+  CreateShortestPathtHouse(THIRDHOUSE_X, FIRSTHOUSE_Y, CHECKHOUSE)
+  CreateShortestPathtHouse(THIRDHOUSE_X, THIRDHOUSE_Y, ENDHOUSE)
+end
+
+function DrawShortestPathHouse()
+  ShortestPathHouseListDraw()
 end
 
 function DrawShortestPath()
-  ShortestPathHouseListDraw()
+  love.graphics.setColor(0, 0, 0, 255)
+  love.graphics.rectangle("fill", FIRSTHOUSE_X+HOUSE_SIZE, FIRSTHOUSE_Y+HOUSE_HALFSIZE+5, PATH_X, PATH_SIZE)
+  love.graphics.rectangle("fill", SECONDHOUSE_x+HOUSE_SIZE, FIRSTHOUSE_Y+HOUSE_HALFSIZE+5, PATH_X, PATH_SIZE)
+  love.graphics.rectangle("fill", FIRSTHOUSE_X+HOUSE_SIZE, SECONDHOUSE_Y+HOUSE_HALFSIZE+5, PATH_X, PATH_SIZE)
+  love.graphics.rectangle("fill", FIRSTHOUSE_X+HOUSE_SIZE, THIRDHOUSE_Y+HOUSE_HALFSIZE+5, PATH_X*2+HOUSE_SIZE, PATH_SIZE)
+
+  love.graphics.rectangle("fill", FIRSTHOUSE_X+HOUSE_HALFSIZE-2, FIRSTHOUSE_Y+HOUSE_SIZE, PATH_SIZE, PATH_Y)
+  love.graphics.rectangle("fill", SECONDHOUSE_x+HOUSE_HALFSIZE-2, FIRSTHOUSE_Y+HOUSE_SIZE, PATH_SIZE, PATH_Y)
+  love.graphics.rectangle("fill", THIRDHOUSE_X+HOUSE_HALFSIZE-2, FIRSTHOUSE_Y+HOUSE_SIZE, PATH_SIZE, PATH_Y*2+HOUSE_SIZE)
+  love.graphics.rectangle("fill", FIRSTHOUSE_X+HOUSE_HALFSIZE-2, SECONDHOUSE_Y+HOUSE_SIZE, PATH_SIZE, PATH_Y)
+
+  love.graphics.polygon("fill", SECONDHOUSE_x-3+HOUSE_SIZE+5, SECONDHOUSE_Y-3+HOUSE_SIZE,
+                                SECONDHOUSE_x-3+HOUSE_SIZE, SECONDHOUSE_Y-3+HOUSE_SIZE+5,
+                                THIRDHOUSE_X+5-5, THIRDHOUSE_Y+10,
+                                THIRDHOUSE_X+5, THIRDHOUSE_Y+10-5)
+  love.graphics.setColor(255, 255, 255, 255)
 end
 
 function ExplainShortestPath()
   love.graphics.setColor(255, 0, 0, 255)
-  love.graphics.print("※조작법※\n 'Up','Dwon','Left','Right' \n선택 : 'Space'",62+285, 13)
-  love.graphics.print("\n",62+285,50)
-  love.graphics.print("\n세 번째 줄",62+285,100)
+  love.graphics.print("※조작법※\n방향 : Up, Dwon, Left, Right\n선택 : Space",62+285, 13)
+  love.graphics.print("\n파란 지붕 집에서 빨간 지붕 집까지",62+285,50)
+  love.graphics.print("\n가장 빠른 길을 선택 하시오.",62+285,100)
   love.graphics.setColor(255, 255, 255, 255)
 end
 
 function StageWinterAlgorithm()
   ExplainShortestPath()
   InitShortestPath()
-  ShortestPathHouseListDraw()
+  DrawShortestPathHouse()
+  DrawShortestPath()
 end
