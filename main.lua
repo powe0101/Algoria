bd800a301bde3c44f07d7c2e225cc5812c64f34c
powe0101 <powe0101@naver.com>
2016-09-11 14:36:13 +0900

    봄 알고리즘 구현 중 90%
    버그 - 화면을 최소로 해둬야 제대로 작동됨.
    - 봄 마왕 스테이지에서 죽고나서 다시 돌아가면  보스토크가 다시 안나옴
    -suit 라이브러리 추가 (버튼,텍스트박스)
    - main,manageheart, springalgorithm

--- main.lua
+++ main.lua
@@ -126,6 +126,11 @@ portalAdmin = true --앞으로는 포탈을 이용해 마음대로 이동할 수
 
 tempForMainXCoord = false
 
+suit = nil
+
+-- storage for text input
+input = {text = ""}
+
 function love.load()
   love.graphics.setBackgroundColor(darkcolor) --배경 색을 지정함
   loadResources() -- 이미지 리소스 불러옴
@@ -231,6 +236,10 @@ function love.update(dt)
   CheckBossMeeting() --중간보스성 내부에서 일정좌표를 넘으면 업데이트를 멈추고 보스와 대화를 나누고 보스 문제를 푸는 단계로 넘어가는 것을 체크함.
   CheckTutorial()
   CheckQmarkAtViilage() --마을에서 느낌표 띄우기.
+
+  if suit ~= nil then
+    CheckSpringAlgorithmAnswer()
+  end
 end
 
 function love.draw()
@@ -303,6 +312,10 @@ function love.draw()
     HeartListDraw() --라이프를 맨 앞에 보이게 하기 위해서 Heart관련만 여기에 그림.
     BheartListDraw()
   end
+
+  if suit ~= nil then
+    suit.draw()
+  end
 end
 
 function SetScale(key,scancode)
@@ -349,6 +362,14 @@ function CheckStartGameForTitle()
   end
 end
 
+function love.textinput(t)
+  if suit == nil then
+    return
+  end
+    -- forward text input to SUIT
+    suit.textinput(t)
+end
+
 function love.keypressed(key,scancode) -- 키입력
   BadEndingContorl()
   ControlBlackSmith()
@@ -366,8 +387,9 @@ function love.keypressed(key,scancode) -- 키입력
   CheckStartGameForTitle() -- 타이틀 키 입력 체크
 
   --문제풀때 오답때 나오는 메시지를 없애기 위함. 0904.현식
-
-
+  if suit ~= nil then
+    suit.keypressed(key)
+  end
   if love.keyboard.isDown("return") then
       splashy.skipSplash()
   end
@@ -503,6 +525,7 @@ function drawGame()
   if pl then
     pl:draw() -- 플레이어 스프라이트 그리기
   end
+
 end
 
 function loadResources()
