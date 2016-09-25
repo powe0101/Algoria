b4d2921982337a703b175c8699a42714df2a67a4
GeneLee <dlgustlr45@naver.com>
2016-09-25 15:49:28 +0900

    2016.09.25
    - 길이 말한대로 수정(타이틀에 버튼)

--- Title.lua
+++ Title.lua
@@ -19,11 +19,11 @@ function DrawTitleMenu()
 
   love.graphics.draw(titleImg,WIDTH/2-110,16)
   suit.layout:reset(0,0)
-  if suit.Button("게임시작", WIDTH/2-100,HEIGHT/2+20,200,15).hit then
+  if suit.Button("게임시작", WIDTH/2-100,HEIGHT/2+20,200,15).hit and nonLoad == false  then
     CheckStartGameForTitle()
   end
 
-  if suit.Button("불러오기", WIDTH/2-100,HEIGHT/2+20+25,200,15).hit then
+  if suit.Button("불러오기", WIDTH/2-100,HEIGHT/2+20+25,200,15).hit and nonLoad == false then
     if love.filesystem.exists( 'savedData.txt' ) then
       LoadGame()
     else
